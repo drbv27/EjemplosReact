4030eed55ad47c180144172a0d8ac19e4653408d
Diego <drbv27@gmail.com>
2021-08-28 09:25:26 -0500

    First component

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Functional Components
+
+const PrimeraApp = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Mi primera aplicacion</p>
+    </>
+  );
+};
+
+export default PrimeraApp;
