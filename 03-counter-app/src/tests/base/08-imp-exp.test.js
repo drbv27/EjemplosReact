3fddc93e7b3c77410fb85ed977d099b5b794f1da
Diego <drbv27@gmail.com>
2021-09-01 08:41:28 -0500

    My second test

---
+++ 03-counter-app/src/tests/base/08-imp-exp.test.js
@@ -0,0 +1,31 @@
+import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
+import heroes from "../../data/heroes";
+
+describe("Pruebas en funciones de heroes", () => {
+  test("should return heroe y id", () => {
+    const id = 1;
+    const heroe = getHeroeById(id);
+    const heroeData = heroes.find((h) => h.id === id);
+    expect(heroe).toEqual(heroeData);
+  });
+  test("should undefined if heroe doesnt exist", () => {
+    const id = 10;
+    const heroe = getHeroeById(id);
+
+    expect(heroe).toBe(undefined);
+  });
+  test("debe retornar un arreglo con los heroes de DC", () => {
+    const owner = "DC";
+    const heroes = getHeroesByOwner(owner);
+
+    const heroesData = heroes.filter((h) => h.owner === owner);
+
+    expect(heroes).toEqual(heroesData);
+  });
+  test("debe retornar un arreglo con los heroes de Marvel", () => {
+    const owner = "Marvel";
+    const heroes = getHeroesByOwner(owner);
+
+    expect(heroes.length).toEqual(2);
+  });
+});
