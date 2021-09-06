12da66c64a789bb8f51f42b81deaae8d4d52dd29
Diego <drbv27@gmail.com>
2021-09-06 09:57:48 -0500

    Implementing the click tests

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -3,9 +3,14 @@ import { shallow } from "enzyme";
 import "@testing-library/jest-dom";
 
 describe("Debe de mostrar <CounterApp /> correctamente (hacer match con el snapshot) y sus valores por defecto.", () => {
-  test("Debe de mostrar <PrimeraApp/> corectamente", () => {
-    const wrapper = shallow(<CounterApp />);
+  let wrapper;
+  wrapper = shallow(<CounterApp />); //this is a bad parctice, but do it for maintain the intellisense
 
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
+  test("Debe de mostrar <PrimeraApp/> corectamente", () => {
     expect(wrapper).toMatchSnapshot();
   });
   test("debe de mostrar el valor por defecto de 100", () => {
@@ -14,5 +19,26 @@ describe("Debe de mostrar <CounterApp /> correctamente (hacer match con el snaps
     const counterText = wrapper.find("h2").text().trim();
 
     expect(counterText).toBe("100");
+  });
+
+  test("Debe de incrementar con el botón de +1", () => {
+    wrapper.find("button").at(0).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("1");
+  });
+  test("Debe de decrementar con el botón de -1", () => {
+    wrapper.find("button").at(2).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("-1");
+  });
+  test("Debe de colocar el valor por defecto de los props", () => {
+    const wrapper = shallow(<CounterApp value={105} />);
+    wrapper.find("button").at(0).simulate("click");
+    wrapper.find("button").at(0).simulate("click");
+    wrapper.find("button").at(1).simulate("click");
+    const counterText = wrapper.find("h2").text().trim();
+    expect(counterText).toBe("105");
   });
 });
